27542f11708ffafc90c2af85ab4d4bdc0f784f8c
Lee, DaeIn <daein.lee@sap.com>
2022-01-14 21:30:54 +0900

    Solve problems

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -2,9 +2,15 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` for hints :)
 
-// I AM NOT DONE
-
+// https://doc.rust-lang.org/reference/macros-by-example.html#the-macro_use-attribute
+// #[macro_use] attribute can be used to make a module's macro
+// scope not end when the module is closed.
+#[macro_use]
 mod macros {
+    // Macros labeled with #[macro_export] are always pub and
+    // can be referred to by other crates, either by path or
+    // by #[macro_use] as described above.
+    // #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,13 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` for hints :)
 
-// I AM NOT DONE
-
+// Note that a reference’s scope starts from where it is introduced and
+// continues through the last time that reference is used.
 fn main() {
     let mut x = 100;
     let y = &mut x;
+    *y += 100; // end of y's scope
     let z = &mut x;
-    *y += 100;
     *z += 1000;
     assert_eq!(x, 1200);
 }
