2f0e96f32fa0b392cc37ae47d47f753cb64a979e
Lee, DaeIn <lazy0ren@gmail.com>
2022-01-24 19:39:09 +0900

    Add another way to solve it

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -22,6 +22,10 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>()?;
+    // let qty = match item_quantity.parse::<i32>() {
+    //     Ok(val) => val,
+    //     Err(e) => return Err(e),
+    // };
 
     Ok(qty * cost_per_item + processing_fee)
 }
